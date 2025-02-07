66369d0081c64fa2c540115136d2c067f65b4cff
wangqiang <478536999@qq.com>
2025-02-07 15:28:26 +0800

    add axios request

---
+++ src/lib/db.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export async function getArticles(uid: string, cursor: number = 0) {
+  const res = await axios.post(
+    "https://api.juejin.cn/content_api/v1/article/query_list",
+    {
+      cursor: cursor + "",
+      sort_type: 2,
+      user_id: uid + "",
+    }
+  );
+
+  return res.data;
+}
+
+export async function getArticleDetail(article_id: string) {
+  const res = await axios.post(
+    "https://api.juejin.cn/content_api/v1/article/detail",
+    {
+      article_id,
+      client_type: 2608,
+    }
+  );
+
+  return res.data;
+}
